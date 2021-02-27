0533693a656eaa225338992840958f81455edcf6
windstonp <joaor4e@gmail.com>
2021-02-27 17:06:23 -0300

    WKUP-05-created-levelup-modal-and-change-level-info-to-cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import '../styles/global.css';
-import { ChallengesProvider } from '../contexts/challengesContext'
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-        <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
